67f531a380f378faa10693247c905c16dc171081
tarioto <timarioto@gmail.com>
2016-09-14 15:43:54 -0700

    Make all tests pass for stack prototypal pattern

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,26 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance.counter = 0;
+  someInstance.storage = {};
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    this.storage[this.counter++] = value;
+  },
+
+  pop: function() {
+    if (this.counter > 0) {
+      return this.storage[--this.counter];
+    }
+  },
+
+  size: function() {
+    return this.counter;
+  }
+};
 
 
