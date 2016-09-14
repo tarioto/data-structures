9a1e28f295fc4cb9a00a633320b8d065b6713072
tarioto <timarioto@gmail.com>
2016-09-14 15:23:12 -0700

    make all tests pass for stack functional-shared pattern

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,30 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {
+    push: stackMethods.push,
+    pop: stackMethods.pop,
+    size: stackMethods.size,
+    counter: 0,
+    storage: []
+  };
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function (value) {
+    this.storage[this.counter++] = value;
+  },
+
+  pop: function () {
+    if (this.counter > 0) {
+      return this.storage[--this.counter];
+    }
+  },
+
+  size: function () {
+    return this.counter;
+  }
+};
 
 
