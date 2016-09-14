e8c33a45387f775f04cbae24eea5d8923b6e0de0
tarioto <timarioto@gmail.com>
2016-09-14 15:52:57 -0700

    Implement queue for prototypal pattern

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,25 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+  someInstance.smallest = 0;
+  someInstance.largest = 0;
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.storage[this.largest++] = value;
+  },
+  dequeue: function() {
+    if (this.size() > 0) {
+      return this.storage[this.smallest++];
+    }
+  },
+  size: function() {
+    return this.largest - this.smallest;
+  }
+};
 
 
