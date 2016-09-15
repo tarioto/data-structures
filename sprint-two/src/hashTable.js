a26758a72e39ce1bc4367e037a93835c64eaf015
tarioto <timarioto@gmail.com>
2016-09-15 10:28:41 -0700

    Implement hashtable and make all tests pass

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,44 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var alreadyThere = this._storage.get(index);
+  if (alreadyThere) {
+    var updated = false;
+    alreadyThere.forEach(function(pair) {
+      if (pair[0] === k) {
+        pair[1] = v;
+        updated = true;
+      }
+    });
+    if (updated) {
+      this._storage.set(index, alreadyThere);
+    } else {
+      alreadyThere.push([k, v]);
+      this._storage.set(index, alreadyThere);
+    }
+  } else {
+    this._storage.set(index, [[k, v]]);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var item = this._storage.get(index);
+  if (!item) {
+    return undefined;
+  }
+  var result;
+  item.forEach(function(pair) {
+    if (pair[0] === k) {
+      result = pair[1];
+    }
+  });
+  return result;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, undefined);
 };
 
 
